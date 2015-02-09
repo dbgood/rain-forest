6e8760b7b8889bf344e8a9099bd75ff9aee50b2d
Dominic Brugnano <dbgood@gmail.com>
2015-02-09 16:47:45 -0500

    Creating log in functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,20 +3,17 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
-
   def create
   	@user = User.new(user_params)
   	if @user.save
   		redirect_to products_url, notice: "Signed up!"
   	else
-  		render "new"
-  			
+  		render "new"  			
+    end
   end
-end
 
-private
-def user_params
-	params.require(:user).permit(:email, :password, :password_confirmation)
-	
+  private
+  def user_params
+	 params.require(:user).permit(:email, :password, :password_confirmation)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,24 @@
 Rainforest::Application.routes.draw do
+  #get 'sessions/new'
+
+  #get 'sessions/create'
+
+  #get 'sessions/destroy'
+
   resources :products
   resources :users, only: [:new, :create]
-
+  resources :sessions, only: [:new, :create, :destroy]
  
 end
 
 
  #Rails.application.routes.draw do
+  #get 'sessions/new'
+
+  #get 'sessions/create'
+
+  #get 'sessions/destroy'
+
   #get 'users/new'
 
   #get 'users/create'
