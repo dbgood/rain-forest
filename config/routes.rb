68c391096414afbd3612703f3cd7081fe169c13f
Dominic Brugnano <dbgood@gmail.com>
2015-02-10 18:03:49 -0500

    Writing the reviews routes, controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,31 @@
 Rainforest::Application.routes.draw do
+  #get 'reviews/show'
+
+  #get 'reviews/create'
+
+  #get 'reviews/destroy'
+
   #get 'sessions/new'
 
   #get 'sessions/create'
 
   #get 'sessions/destroy'
 
-  resources :products
-  resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :products do
+  resources :reviews, only: [:show, :create, :destroy]
+  
  
+  end
 end
 
 
  #Rails.application.routes.draw do
+  #get 'reviews/show'
+
+  #get 'reviews/create'
+
+  #get 'reviews/destroy'
+
   #get 'sessions/new'
 
   #get 'sessions/create'
